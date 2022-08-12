fed92762dc2332efff1305dceea74892c1c75ff8
Monnavarro <montserrat.nvro.lpz@gmail.com>
2022-08-12 13:46:01 +0200

    new: Learn the basics of API

---
+++ project_1/perform_ocr_pytesseract.py
@@ -0,0 +1,16 @@
+# -- coding: utf-8 --
+"""
+
+Created on: 12/8/22
+@author: Montse Navarro <montserrat.nvro.lpz@gmail.com>
+Licence,
+"""
+import pytesseract
+
+# pytesseract.pytesseract.tesseract_cmd = "C:\\Program Files\\Tesseract-OCR\\tesseract.exe"
+
+img_path = "test.png"
+lang = "eng"
+text = pytesseract.image_to_string(img_path, lang=lang)
+
+print(text)
